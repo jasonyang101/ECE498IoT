e394ec399c04737c94c1115812ed5933ab8c7bea
Jason Yang <jyang@Jasons-MacBook-Pro.local>
2019-02-26 03:58:39 -0600

    adding in part 2 and screenshot proof of everything working

---
+++ lab2/mp2part2.py
@@ -0,0 +1,40 @@
+import tensorflow
+from tensorflow import keras
+import numpy as np
+import requests
+import gzip
+
+url = 'https://courses.engr.illinois.edu/ece498icc/sp2019/lab2_request_dataset.php'
+
+def get_testset():
+    values = {'request': 'testdata', 'netid':'jyang223'}
+    r = requests.post(url, data=values, allow_redirects=True)
+    filename = r.url.split("/")[-1]
+    testset_id = filename.split(".")[0].split("_")[-1]
+    with open(filename, 'wb') as f:
+        f.write(r.content)
+    return load_dataset(filename), testset_id
+
+def load_dataset(path):
+    num_img = 1000
+    with gzip.open(path, 'rb') as infile:
+        data = np.frombuffer(infile.read(), dtype=np.uint8).reshape(num_img, 28,28)
+    return data
+
+test_images, id = get_testset()
+test_images = np.expand_dims(test_images, axis=3)
+new_model = keras.models.load_model('keras_model.h5')
+new_model.summary()
+out = []
+predictions = new_model.predict(test_images)
+for i in range(len(predictions)):
+    out.append(np.argmax(predictions[i]))
+predict = ""
+for i in range(len(out)):
+    predict += str(out[i])
+print predict
+
+values = {'request': 'verify', 'netid':'jyang223', 'testset_id':id,'prediction':predict}
+r_2 = requests.post(url,data=values,allow_redirects=True)
+print r_2.text
+print "Percentage:" + str(float(int(r_2.text)/1000.0))
